95231f424c52b80336c19b7fcd32783baebdd578
Technifutur <600@technifutur.be>
2017-11-29 15:26:40 +0100

    First commit

---
+++ Assets/Game_of_life.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Game_of_life : DualBehaviour
+{
+    #region Public Members
+
+    public GameObject Plan;
+    public 
+
+    #endregion
+
+    #region Publuc void
+
+    #endregion
+
+    #region system
+
+    void Start () //pas de logique dans start et update
+    {
+        Debug.Log("I am alive!");
+
+    }
+	
+	
+	void Update () 
+    {
+		
+	}
+
+    #endregion
+
+    #region Tools Debug and Utility
+
+    #endregion
+
+    #region Private and Protected Members
+
+    #endregion
+}
